610d164ad028a8fc0809842024e7b92335a77717
mrmrinal <mrinalganesh@gmail.com>
2022-01-07 11:54:26 +0800

    Foundation 1

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/native-stack';
+import SignupScreen from './screens/loggedout/SignUp';
+import LoginScreen from './screens/loggedout/Login';
+import HomeScreen from './screens/loggedin/HomeScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Stack = createStackNavigator();
+
+const AuthStack = () => {
+    const [isFirstLaunch, setIsFirstLaunch] = useState(null);
+    let routeName;
+
+    useEffect(() => {
+        AsyncStorage.getItem('alreadyLaunched').then((value) => {
+            if (value === null) {
+                AsyncStorage.setItem('alreadyLaunched', 'true');
+                setIsFirstLaunch(true);
+            } else {
+                setIsFirstLaunch(false);
+            }
+        });
+    }, []);
+
+    if (isFirstLaunch === null) {
+        return null;
+    } else if (isFirstLaunch === true) {
+        routeName = 'Signup';
+    } else {
+        routeName = 'Login';
+    }
+
+    return (
+        <Stack.Navigator initialRouteName={routeName}>
+            <Stack.Screen
+                name="Login"
+                component={LoginScreen}
+                options={{ header: () => null }}
+            />
+            <Stack.Screen
+                name="Signup"
+                component={SignupScreen}
+            />
+            <Stack.Screen
+              name="HomeScreen"
+              component={HomeScreen}
+            />
+        </Stack.Navigator>
+    );
+};
+
+export default AuthStack;
